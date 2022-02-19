ce8c30b2846c3486f422fa5d0206e437c33e01a9
Boris Slav <boris@slav.rocks>
2022-02-19 12:04:29 +0000

    create scrapper class

--- apps/scrapper/main.py
+++ apps/scrapper/main.py
@@ -1,17 +1,30 @@
-from fastapi import FastAPI
-from binance.spot import Spot
+from fastapi import FastAPI, HTTPException
 from tbmods.config import Config
+from scrapper import Scrapper
 
+## init api
 app = FastAPI(
     title="Scrapper",
     description="Scrap financial data from binance and store it in questdb",
     version="1.0.0"
 )
 
+## init dependencies
 config = Config()
-binance_client = Spot(key=config['binance_api_key'], secret=config['binance_api_secret'])
+scrapper = Scrapper(config)
 
+## scrap
 @app.get("/scrap")
-async def scrap():
-    trades = binance_client.historical_trades(config['symbol'],limit=config['binance_hist_limit'],fromId=0)
-    return "scrap"
+def scrap():
+    """
+    Scrap trades from last inserted into questdb to binance_hist_limit and insert result into questdb table trades_symbol
+    This is not async because we check on database last inserted row, so we have to wait db write
+    """
+    # Getting last id ingested
+    if scrapper.get_last_id() is False: HTTPException(status_code=500, detail="Unable to get last_id")
+    # Getting 1000 following last id trades
+    if scrapper.get_trades() is False: HTTPException(status_code=500, detail="Unable to get trades")
+    # Insert them all into questDB
+    if scrapper.ingest_trades() is False: HTTPException(status_code=500, detail="Unable to ingest trades")
+    # Return number ingested counts
+    return {"scrapping":"Done"}
