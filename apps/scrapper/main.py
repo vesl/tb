dcdac1b7795164e7692f7f57e57958a92a521da7
Boris Slav <boris@slav.rocks>
2022-02-21 22:42:09 +0100

    Store all binance trades is too long and cost too much storage. Rework scrapper to pull 1 min trades and send it to candles for storage. Candles can be scaled out and manage storage async while scrapper scraps from binance. V 1.1.0

--- apps/scrapper/main.py
+++ apps/scrapper/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, HTTPException
-from tbmods.config import Config
-from scrapper import Scrapper
+from fastapi import FastAPI
+from .routers import trades
 
 ## init api
 app = FastAPI(
@@ -8,23 +7,5 @@ app = FastAPI(
     description="Scrap financial data from binance and store it in questdb",
     version="1.0.0"
 )
-
-## init dependencies
-config = Config()
-scrapper = Scrapper(config)
-
-## scrap
-@app.get("/scrap")
-def scrap():
-    """
-    Scrap trades from last inserted into questdb to binance_hist_limit and insert result into questdb table trades_symbol
-    This is not async because we check on database last inserted row, so we have to wait db write
-    """
-    # Getting last id ingested
-    if scrapper.get_last_id() is False: HTTPException(status_code=500, detail="Unable to get last_id")
-    # Getting 1000 following last id trades
-    if scrapper.get_trades() is False: HTTPException(status_code=500, detail="Unable to get trades")
-    # Insert them all into questDB
-    if scrapper.ingest_trades() is False: HTTPException(status_code=500, detail="Unable to ingest trades")
-    # Return number ingested counts
-    return {"scrapping":"Done"}
+## init routers
+app.include_router(trades.router)
