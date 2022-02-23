6f4a2577ef26ffdcd77fb37b4a6c5bf99045d0d9
Boris Slav <boris@slav.rocks>
2022-02-23 21:21:14 +0000

    Major upgrade. * remove candles api and use it as module tbmods.candles * scrapper push data into questdb using influxdb protocol

--- apps/scrapper/routers/trades.py
+++ apps/scrapper/routers/trades.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, HTTPException
+from binance.spot import Spot
+from tbmods.candles import Candles
 from tbmods.config import Config
 from tbmods.cache import Cache
 from tbmods.log import Log
-from binance.spot import Spot
-from datetime import datetime,timedelta
-import requests
 
 router = APIRouter(
     prefix="/trades",
@@ -13,51 +12,41 @@ router = APIRouter(
 
 config = Config()
 log = Log(config['app'])
-cache = Cache(config['app'])
 
-@router.get('/binance')
-def get_binance_trades():
+@router.get('/')
+def scrap_trades():
     """
-    Purpose: collect 1 minute of trades from binance and send it to candles
-    Steps:
-    * get last_id from cache
-    * get 1 trade from binance and compute current minute, then the next minute
-    * get trades from binance while they are before the next minute
-    * filter trades after the next minute (pull packs of binance_hist_limit from binance)
-    * send it to candles
-    *** This is not async to avoid race condition on cache
+    Purpose: get binance trades and ingest it in questdb
+    * Decrease config['binance_trades_pack_size'] to pull new trades faster, or Increase to pull history faster
     """
+
+    # init cache and get last id
+    cache = Cache(config['app'])
     last_id = cache.data['trades/binance/last_id'] if 'trades/binance/last_id' in cache.data else -1
     log.info('Last id is {}'.format(last_id))
     
+    # init binance client and get 10 packs of 1000 trades to be sure to get at least 2 candles (we drop last one)
     binance_client = Spot(key=config['binance_api_key'], secret=config['binance_api_secret'])
-    trades = binance_client.historical_trades(config['symbol'],limit=1,fromId=last_id+1)
-    if len(trades) == 0: raise HTTPException(status_code=425,detail="Too early for first trade")
+    trades = []
+    while len(trades) < config['binance_trades_pack_size']:
+        trades.extend(binance_client.historical_trades(config['symbol'],limit=config['binance_hist_limit'],fromId=last_id+1))
+        if len(trades) < config['binance_hist_limit']: raise HTTPException(status_code=425,detail="First pack size is {}".format(len(trades)))
+        last_id = trades[-1]['id']
+    log.info("Got {} trades".format(len(trades)))
     
-    # trick , because datetime cant handle millisecond timestamps
-    dt = datetime(1970, 1, 1) + timedelta(milliseconds=trades[0]['time'])
-    log.info('First trade is at {}'.format(dt))
-    ts_end = int(datetime.timestamp(dt + (datetime.min - dt) % timedelta(minutes=1))*1000)
-    ts = int(datetime.timestamp(dt)*1000)
+    # init candles class 
+    candles = Candles()
+    last_id = candles.from_trades(trades)
+    if not last_id:
+        log.error("Maybe minute is not finished, try again later")
+        raise HTTPException(status_code=425,detail="Maybe minute is not finished, try again later")
+    ingest = candles.ingest()
+    if 'error' in ingest:
+        log.error("Trades not ingested {}".format(ingest['error']))
+        raise HTTPException(status_code=500,detail="Trades not ingested {}".format(ingest['error']))
     
-    while(ts <= ts_end):
-        pack=binance_client.historical_trades(config['symbol'],limit=config['binance_hist_limit'],fromId=trades[-1]['id']+1)
-        if len(pack) == 0: raise HTTPException(status_code=425,detail="Too early for last trade")
-        trades.extend(pack)
-        ts=trades[-1]['time']
-    
-    trades = list(filter(lambda trade: trade['time'] <= ts_end, trades))
-    log.info('Got {} trades'.format(len(trades)))
-    dt_end = datetime(1970, 1, 1) + timedelta(milliseconds=trades[-1]['time'])
-    log.info('Last trade is at {}'.format(dt_end))
-    
-    try:
-        r = requests.put(config['candles_trades_store_url'],json={"trades":trades})
-    except requests.exceptions.ConnectionError as e:
-        raise HTTPException(status_code=500,detail="Sent to candles failed. {}".format(e))
-    if r.status_code != 200: raise HTTPException(status_code=500,detail="Sent to candles failed")
-    cache.data['trades/binance/last_id'] = trades[-1]['id']
+    # update cache and write
+    cache.data['trades/binance/last_id'] = last_id
     cache.write()
-    return {"Scrapped": "from {} to {}".format(dt,dt_end)}
     
-    
+    return {"ingested":"{} candles".format(len(candles.candles))}
--- tbmods/questdb/__init__.py
+++ tbmods/questdb/__init__.py
@@ -1,4 +1,8 @@
+from tbmods.config import Config
 import requests
+import socket
+
+config = Config()
 
 class QuestDB:
     """
@@ -11,7 +15,7 @@ class QuestDB:
     Processing error and result key outside this class
     """
     
-    def __init__(self,config):
+    def __init__(self):
         self.host = config['questdb_host']
         self.table_historical_trades = 'historical_trades'
             
@@ -22,10 +26,25 @@ class QuestDB:
         query_response = {}
         params = {"query":query,"fmt":"json"}
         try:
-            response = requests.get("http://{}/exec".format(self.host),params=params).json()
+            response = requests.get("http://{}:9000/exec".format(self.host),params=params).json()
             if 'error' in response: query_response['error'] = response['error']
             if 'dataset' in response: query_response['result'] = response['dataset']
         except requests.exceptions.RequestException as e:
             query_response['error'] = e
+        return query_response
+        
+    def ingest(self,data):
+        """
+        Ingest using influxdb protocol
+        """
+        query_response = {}
+        data = "\n".join(data)+"\n"
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.connect((self.host, 9009))
+            sock.sendall(data.encode())
+        except socket.error as e:
+            query_response['error'] = e
+        sock.close()
         return query_response
             
