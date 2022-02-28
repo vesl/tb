450f8dd9a51808ad9fc0b00b3a94a40fb7602c38
Boris Slav <boris@slav.rocks>
2022-02-28 22:38:56 +0000

    factorise status.js

--- apps/front/js/status.js
+++ apps/front/js/status.js
@@ -1,15 +1,7 @@
-function statusContent(status) {
-    var status = JSON.stringify(status,undefined, 2)
-    $('#content').html('<pre class="bg-light text-dark rounded">'+status+'</pre>')
-}
-
-function statusTitle(title) {
-    $('#content-title').text(title+' status')
-}
-
 function showStatus(app){
-    statusTitle(app)
-    return $.get("/api/"+app+"/status/",function(status){
-        statusContent(status)
+    $('#content-title').text(app+' status')
+    $.get("/api/"+app+"/status/",function(status){
+        var status = JSON.stringify(status,undefined, 2)
+        $('#content').html('<pre class="bg-light text-dark rounded">'+status+'</pre>')
     })
 }
