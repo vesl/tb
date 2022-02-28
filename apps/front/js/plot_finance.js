6f4eee05ea7c98063252ed955576061c265cbb3d
Boris Slav <boris@slav.rocks>
2022-02-28 18:49:41 +0000

    implement volume + rsi into front

--- apps/front/js/plot_finance.js
+++ apps/front/js/plot_finance.js
@@ -4,6 +4,10 @@ function showPlotFinance(plot){
     switch(plot){
         case 'ohlc':
             var submitFunction = function(){plotFinanceOhlc()}
+            break
+        case 'volume':
+            var submitFunction = function(){plotFinanceVolume()}
+            break
     }
     $('#button-plot').click(submitFunction)
 }
@@ -18,5 +22,31 @@ function plotFinanceOhlc(){
         const candlestickSeries = chart.addCandlestickSeries();
         var data = JSON.parse(data)
         candlestickSeries.setData(data)
+    })
+}
+
+function plotFinanceVolume(){
+    $('#plot').html(loading())
+    values = getFormValues()
+    if (values == false) { return false}
+    $.get('/api/plotter/finance/volume/'+values['timescale']+'/'+values['from']+'/'+values['to'],function(data){
+        $('#plot').empty()
+        const chart = LightweightCharts.createChart(document.getElementById('plot'));
+        const volumeSeries = chart.addHistogramSeries();
+        var data = JSON.parse(data)
+        volumeSeries.setData(data)
+    })
+}
+
+function plotFinanceRsi(){
+    $('#plot').html(loading())
+    values = getFormValues()
+    if (values == false) { return false}
+    $.get('/api/plotter/finance/rsi/'+values['timescale']+'/'+values['from']+'/'+values['to'],function(data){
+        $('#plot').empty()
+        const chart = LightweightCharts.createChart(document.getElementById('plot'));
+        const rsiSeries = chart.addLineSeries();
+        var data = JSON.parse(data)
+        rsiSeries.setData(data)
     })
 }
