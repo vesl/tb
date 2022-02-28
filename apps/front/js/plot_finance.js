e0170f79a6cb7ca51475c959732a5147ceeff0dc
Boris Slav <boris@slav.rocks>
2022-02-28 13:35:28 +0000

    rename price to ohlc into front

--- apps/front/js/plot_finance.js
+++ apps/front/js/plot_finance.js
@@ -2,36 +2,21 @@ function showPlotFinance(plot){
     plotTitle('finance '+plot)
     initPlotSkeleton()
     switch(plot){
-        case 'price':
-            var submitFunction = function(){plotFinancePrice()}
+        case 'ohlc':
+            var submitFunction = function(){plotFinanceOhlc()}
     }
     $('#button-plot').click(submitFunction)
 }
 
-function plotFinancePrice(){
+function plotFinanceOhlc(){
     $('#plot').html(loading())
     values = getFormValues()
     if (values == false) { return false}
-    $.get('/api/plotter/finance/price/'+values['timescale']+'/'+values['from']+'/'+values['to'],function(data){
+    $.get('/api/plotter/finance/ohlc/'+values['timescale']+'/'+values['from']+'/'+values['to'],function(data){
         $('#plot').empty()
         const chart = LightweightCharts.createChart(document.getElementById('plot'));
-        const volumeSeries = chart.addHistogramSeries({
-            color: '#AEAEAE',
-            lineWidth: 2,
-            priceFormat: {type: 'volume'},
-            overlay: true,
-            scaleMargins: {
-                top: 0.8,
-                bottom: 0,
-            }
-        });
         const candlestickSeries = chart.addCandlestickSeries();
         var data = JSON.parse(data)
         candlestickSeries.setData(data)
-        volumeData = []
-        data.forEach(function(candle){
-            volumeData.push({'time':candle['time'],'value':candle['volume']})
-        })
-        volumeSeries.setData(volumeData)
     })
 }
