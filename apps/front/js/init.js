887e6b977fd3e6b99952526f670b0d4f58f061cd
Boris Slav <boris@slav.rocks>
2022-03-02 11:52:41 +0000

    rework plot to get better structure and more handful charts

--- apps/front/js/init.js
+++ apps/front/js/init.js
@@ -1,11 +1,31 @@
+function initPlotLoading(){
+    $.get('/loading.html',function(html){$('#plot').html(html)})
+}
+
+function initPlotForm(next){
+    $.get('/plot.html',function(html){
+        $('#content').html(html)
+        $('#datepicker-from').datepicker({format:'yyyy-mm-dd'})
+        $('#datepicker-to').datepicker({format:'yyyy-mm-dd'})
+        next()
+    })
+}
+
+function initPlotDiv(){
+    divPlot = $('<div>')
+    divPlot.attr('id','plot')
+    divPlot.addClass('min-50')
+    $('#content').append(divPlot)
+}
+
 window.onload = function () {
-    
+
     for (const app in nav) {
-    
+
         const appLi = $('<li>')
         const appButton = $('<button>')
         const appDiv = $('<div>')
-    
+
         appLi.addClass('mb-1')
         appButton.addClass('btn btn-toggle align-items-center rounded collapsed')
         appButton.attr('data-bs-toggle','collapse')
@@ -14,7 +34,7 @@ window.onload = function () {
         appButton.text(app)
         appDiv.addClass('collapse')
         appDiv.attr('id',app.toLowerCase())
-    
+
         for (const menu in nav[app]) {
             const ulMenu = $('<ul>')
             const liMenu = $('<li>')
@@ -33,5 +53,44 @@ window.onload = function () {
         appLi.append(appButton)
         appLi.append(appDiv)
         $('#nav').append(appLi)
+    }
+}
+
+function initPlotIndicatorsForm(){
+
+    const indLi = $('<li>')
+    const indButton = $('<button>')
+    const indDiv = $('<div>')
+
+    indLi.addClass('mb1')
+    indButton.addClass('btn btn-toggle align-items-center rounded collapsed bg-white')
+    indButton.attr('data-bs-toggle','collapse')
+    indButton.attr('data-bs-target','#indicators-list')
+    indButton.attr('aria-expanded','false')
+    indButton.text('Indicators')
+    indDiv.addClass('collapse')
+    indDiv.attr('id','indicators-list')
+
+    indLi.append(indButton)
+    indLi.append(indDiv)
+    $('#indicators-select').append(indLi)
+
+    for (const indicator in indicators){
+        const divCheckBox = $('<div>')
+        const inputCheckBox = $('<input>')
+        const labelCheckBox = $('<label>')
+
+        divCheckBox.addClass('form-check form-check-inline')
+        inputCheckBox.addClass('form-check-input')
+        inputCheckBox.attr('id','checkbox-indicator-'+indicator)
+        inputCheckBox.attr('type','checkbox')
+        inputCheckBox.attr('value',indicator)
+        labelCheckBox.addClass('form-check-label')
+        labelCheckBox.attr('for','checkbox-indicator-'+indicator)
+        labelCheckBox.text(indicator)
+
+        divCheckBox.append(inputCheckBox)
+        divCheckBox.append(labelCheckBox)
+        indDiv.append(divCheckBox)
     }
 }
--- apps/front/js/plot.js
+++ apps/front/js/plot.js
@@ -1,14 +1,15 @@
-function loading(){
-    $.get('/loading.html',function(html){$('#plot').html(html)})
+var charts = {}
+
+function showPlotsFinance(){
+    initPlotForm(function(){
+        initPlotIndicatorsForm()
+        initPlotDiv()
+        $('#button-plot').click(function(){requestFinancePlotData()})
+    })
+    $('#content-title').html('Finance plots')
 }
 
-function initForm(){
-    $.get('/plot.html',function(html){$('#content').html(html)})
-    $('#datepicker-from').datepicker({format:'yyyy-mm-dd'})
-    $('#datepicker-to').datepicker({format:'yyyy-mm-dd'})
-}
-
-function getFormValues(){
+function getFormPlotValues(){
     var values = {}
     values['from'] = $('#datepicker-from').val()
     values['to'] = $('#datepicker-to').val()
@@ -16,7 +17,46 @@ function getFormValues(){
     if(values['from'] == undefined || values['to'] == undefined || values['timescale'] == 'Timescale'){
         alert('Fill the form')
         return false
-    } else {
-        return values
     }
+    else return values
+}
+
+function getFormIndicators(){
+    let checkedIndicators = []
+    for(const indicator in indicators){
+        if ($('#checkbox-indicator-'+indicator).is(':checked') === true) { checkedIndicators.push(indicator)}
+    }
+    return checkedIndicators
+}
+
+function requestFinancePlotData(){
+    const values = getFormPlotValues()
+    const checkedIndicators = getFormIndicators()
+    if (values === false) return false
+    initPlotLoading()
+    $.get('/api/plotter/finance/candles/'+values['timescale']+'/'+values['from']+'/'+values['to'],function(data){
+        $('#plot').empty()
+        charts['price'] = LightweightCharts.createChart(document.getElementById('plot'));
+        const candlesSeries = charts['price'].addCandlestickSeries()
+        candlesSeries.setData(JSON.parse(data))
+        checkedIndicators.forEach(function(indicator){
+            indicators[indicator](values)
+        })
+    })
+}
+
+function plotFinanceVolume(values){
+    $.get('/api/plotter/finance/volume/'+values['timescale']+'/'+values['from']+'/'+values['to'],function(data){
+        const volumeSeries = charts['price'].addHistogramSeries({
+            color: '#aeaeae',
+            lineWidth: 2,
+            priceFormat: {type: 'volume'},
+            overlay: true,
+            scaleMargins: {
+                top: 0.8,
+                bottom: 0,
+            }
+        })
+        volumeSeries.setData(JSON.parse(data))
+    })
 }
