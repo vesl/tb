b17c3518285dbe24a40ae5fd17103a33db132e4c
Boris Slav <boris@slav.rocks>
2022-03-02 21:24:54 +0000

    rework features build indicators

--- apps/dataset/routers/features.py
+++ apps/dataset/routers/features.py
@@ -11,11 +11,20 @@ router = APIRouter(
 
 config = Config()
 log = Log(config['app'])
-indicators = Indicators()
 
 @router.get('/{features}/{timescale}/{from_date}/{to_date}')
 async def get_features(features,timescale,from_date,to_date):
+    """
+    Load ohlc from indicators, then add extra features
+    """
+    # split features
     features = features.split(',')
+    # init indicators and load basic features (ohlc+volume)
+    indicators = Indicators()
     qdbr = indicators.candles_from_questdb(timescale,from_date,to_date)
     if 'error' in qdbr: return qdbr
-    return indicators.candles[features]
+    # load extra features
+    for feature in features:
+        if feature not in indicators.candles.columns:
+            if indicators.load_indicator(feature) is False: return {'error':'Unable to load {} feature'.format(feature)}
+    return indicators.candles[features].to_json()
