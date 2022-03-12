805e27e6b5d14004ae46b1a25d5622fb7cfbd4df
Boris Slav <boris@slav.rocks>
2022-03-12 10:33:44 +0000

    handle upgrade / install into install_telegraf.sh

--- charts/telegraf/install_telegraf.sh
+++ charts/telegraf/install_telegraf.sh
@@ -3,5 +3,6 @@
 for instance in $(ls values.telegraf-*.yaml)
 do
 	release_name=$(echo $instance | awk -F. '{print $2}')
-	$(helm list | grep -q $release_name) || helm install $release_name -f $instance influxdata/telegraf
+	$(helm list | grep -q $release_name)  && action=upgrade || action=install
+        helm $action $release_name -f $instance influxdata/telegraf
 done
