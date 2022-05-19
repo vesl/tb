bbf1c77bc4a832c3e30fec0b7d666c59c3317f69
Boris Slav <boris@slav.rocks>
2022-05-19 06:40:17 +0000

    use createSubPlotLc instead of createSubPlot for LC

--- apps/front/js/plot.js
+++ apps/front/js/plot.js
@@ -1,14 +1,14 @@
+// Finance plots
 function setupPlotFinance(){
     plot = new PlotLc('Finance plots',plotFinance)
     plot.show()
 }
 
-// Finance plots
 function plotFinance(plot){
     dates = plot.getDates()
     if (dates == false) return
     // create ohlc + volume chart anyway
-    plot.createSubPlot('ohlc','finance','open,high,low,close,volume',dates,function(plotDiv,data){
+    plot.createSubPlotLc('ohlc','finance','open,high,low,close,volume',dates,function(plotDiv,data){
         const chartsOhlc = plot.createLcChart(plotDiv,400)
         const ohlcSeries = chartsOhlc.addCandlestickSeries()
         const volumeSeries = chartsOhlc.addHistogramSeries({
@@ -31,16 +31,15 @@ function plotFinance(plot){
 }
 
 function plotFinanceRSI(plot,dates){
-    plot.createSubPlot('rsi','finance','rsi',dates,function(plotDiv,data){
+    plot.createSubPlotLc('rsi','finance','rsi',dates,function(plotDiv,data){
         const chartRSI = plot.createLcChart(plotDiv)
         const rsiSeries = chartRSI.addLineSeries({color:"#4B7BFD"})
         rsiSeries.setData(plot.extractKeyAsValue(data,'rsi'))
     })
-    // Plotter ne retourne pas la clé RSI
 }
 
 function plotFinanceMACD(plot,dates){
-    plot.createSubPlot('macd','finance','macd',dates,function(plotDiv,data){
+    plot.createSubPlotLc('macd','finance','macd',dates,function(plotDiv,data){
         const chartMACD = plot.createLcChart(plotDiv)
         const macdSeries = chartMACD.addLineSeries({color:'#FF7070'})
         const macdSignalSeries = chartMACD.addLineSeries({color:'#FFBC70'})
@@ -61,7 +60,7 @@ function plotFinanceMACD(plot,dates){
 }
 
 function plotFinanceADX(plot,dates){
-    plot.createSubPlot('adx','finance','adx',dates,function(plotDiv,data){
+    plot.createSubPlotLc('adx','finance','adx',dates,function(plotDiv,data){
         const chartADX = plot.createLcChart(plotDiv)
         const adxSeries = chartADX.addLineSeries({color:'#F3003A'})
         const adxrSeries = chartADX.addLineSeries({color:'#00F33A'})
@@ -71,7 +70,7 @@ function plotFinanceADX(plot,dates){
 }
 
 function plotFinanceAPO(plot,dates){
-    plot.createSubPlot('apo','finance','apo',dates,function(plotDiv,data){
+    plot.createSubPlotLc('apo','finance','apo',dates,function(plotDiv,data){
         const chartAPO = plot.createLcChart(plotDiv)
         const apoSeries = chartAPO.addLineSeries({color:"#0051FE"})
         apoSeries.setData(plot.extractKeyAsValue(data,'apo'))
@@ -79,7 +78,7 @@ function plotFinanceAPO(plot,dates){
 }
 
 function plotFinanceAroon(plot,dates){
-    plot.createSubPlot('aroon','finance','aroon',dates,function(plotDiv,data){
+    plot.createSubPlotLc('aroon','finance','aroon',dates,function(plotDiv,data){
         const chartAROON = plot.createLcChart(plotDiv)
         const aroonUpSeries = chartAROON.addLineSeries({color:"#FE00DB"})
         const aroonDownSeries = chartAROON.addLineSeries({color:"#9E00FE"})
@@ -91,7 +90,7 @@ function plotFinanceAroon(plot,dates){
 }
 
 function plotFinanceBOP(plot,dates){
-    plot.createSubPlot('bop','finance','bop',dates,function(plotDiv,data){
+    plot.createSubPlotLc('bop','finance','bop',dates,function(plotDiv,data){
         const chartBOP = plot.createLcChart(plotDiv)
         const bopSeries = chartBOP.addLineSeries({color:"#C1AA43"})
         bopSeries.setData(plot.extractKeyAsValue(data,'bop'))
@@ -99,7 +98,7 @@ function plotFinanceBOP(plot,dates){
 }
 
 function plotFinanceCCI(plot,dates){
-     plot.createSubPlot('cci','finance','cci',dates,function(plotDiv,data){
+     plot.createSubPlotLc('cci','finance','cci',dates,function(plotDiv,data){
         const chartCCI = plot.createLcChart(plotDiv)
         const cciSeries = chartCCI.addLineSeries({color:"#005005"})
         cciSeries.setData(plot.extractKeyAsValue(data,'cci'))
@@ -107,7 +106,7 @@ function plotFinanceCCI(plot,dates){
 }
 
 function plotFinanceCMO(plot,dates){
-    plot.createSubPlot('cmo','finance','cmo',dates,function(plotDiv,data){
+    plot.createSubPlotLc('cmo','finance','cmo',dates,function(plotDiv,data){
         const chartCMO = plot.createLcChart(plotDiv)
         const cmoSeries = chartCMO.addLineSeries({color:"#959595"})
         cmoSeries.setData(plot.extractKeyAsValue(data,'cmo'))
@@ -115,7 +114,7 @@ function plotFinanceCMO(plot,dates){
 }
 
 function plotFinanceDX(plot,dates){
-    plot.createSubPlot('dx','finance','dx',dates,function(plotDiv,data){
+    plot.createSubPlotLc('dx','finance','dx',dates,function(plotDiv,data){
         const chartDX = plot.createLcChart(plotDiv)
         const dxSeries = chartDX.addLineSeries({color:"#930085"})
         dxSeries.setData(plot.extractKeyAsValue(data,'dx'))
@@ -123,7 +122,7 @@ function plotFinanceDX(plot,dates){
 }
 
 function plotFinanceMFI(plot,dates){
-    plot.createSubPlot('mfi','finance','mfi',dates,function(plotDiv,data){
+    plot.createSubPlotLc('mfi','finance','mfi',dates,function(plotDiv,data){
         const chartDX = plot.createLcChart(plotDiv)
         const dxSeries = chartDX.addLineSeries({color:"#DBE600"})
         dxSeries.setData(plot.extractKeyAsValue(data,'mfi'))
@@ -131,7 +130,7 @@ function plotFinanceMFI(plot,dates){
 }
 
 function plotFinanceMinusDI(plot,dates){
-    plot.createSubPlot('minusdi','finance','minusdi',dates,function(plotDiv,data){
+    plot.createSubPlotLc('minusdi','finance','minusdi',dates,function(plotDiv,data){
         const chartMINUSDI = plot.createLcChart(plotDiv)
         const minusdiSeries = chartMINUSDI.addLineSeries({color:"#0088E6"})
         minusdiSeries.setData(plot.extractKeyAsValue(data,'minusdi'))
@@ -139,7 +138,7 @@ function plotFinanceMinusDI(plot,dates){
 }
 
 function plotFinanceMinusDM(plot,dates){
-    plot.createSubPlot('minusdm','finance','minusdm',dates,function(plotDiv,data){
+    plot.createSubPlotLc('minusdm','finance','minusdm',dates,function(plotDiv,data){
         const chartMINUS = plot.createLcChart(plotDiv)
         const minusdmSeries = chartMINUS.addLineSeries({color:"#83CCFF"})
         minusdmSeries.setData(plot.extractKeyAsValue(data,'minusdm'))
@@ -147,7 +146,7 @@ function plotFinanceMinusDM(plot,dates){
 }
 
 function plotFinanceMOM(plot,dates){
-    plot.createSubPlot('mom','finance','mom',dates,function(plotDiv,data){
+    plot.createSubPlotLc('mom','finance','mom',dates,function(plotDiv,data){
         const chartMOM = plot.createLcChart(plotDiv)
         const momSeries = chartMOM.addLineSeries({color:"#FF8383"})
         momSeries.setData(plot.extractKeyAsValue(data,'mom'))
@@ -155,7 +154,7 @@ function plotFinanceMOM(plot,dates){
 }
 
 function plotFinancePlusDI(plot,dates){
-    plot.createSubPlot('plusdi','finance','plusdi',dates,function(plotDiv,data){
+    plot.createSubPlotLc('plusdi','finance','plusdi',dates,function(plotDiv,data){
         const chartPLUSDI = plot.createLcChart(plotDiv)
         const plusdiSeries = chartPLUSDI.addLineSeries({color:"#00E819"})
         plusdiSeries.setData(plot.extractKeyAsValue(data,'plusdi'))
@@ -163,7 +162,7 @@ function plotFinancePlusDI(plot,dates){
 }
 
 function plotFinancePlusDM(plot,dates){
-    plot.createSubPlot('plusdm','finance','plusdm',dates,function(plotDiv,data){
+    plot.createSubPlotLc('plusdm','finance','plusdm',dates,function(plotDiv,data){
         const chartPLUSDM = plot.createLcChart(plotDiv)
         const plusdmSeries = chartPLUSDM.addLineSeries({color:"#007C0D"})
         plusdmSeries.setData(plot.extractKeyAsValue(data,'plusdm'))
@@ -171,7 +170,7 @@ function plotFinancePlusDM(plot,dates){
 }
 
 function plotFinancePPO(plot,dates){
-    plot.createSubPlot('ppo','finance','ppo',dates,function(plotDiv,data){
+    plot.createSubPlotLc('ppo','finance','ppo',dates,function(plotDiv,data){
         const chartPPO = plot.createLcChart(plotDiv)
         const ppoSeries = chartPPO.addLineSeries({color:"#000F7C"})
         ppoSeries.setData(plot.extractKeyAsValue(data,'ppo'))
@@ -179,7 +178,7 @@ function plotFinancePPO(plot,dates){
 }
 
 function plotFinanceROC(plot,dates){
-    plot.createSubPlot('roc','finance','roc',dates,function(plotDiv,data){
+    plot.createSubPlotLc('roc','finance','roc',dates,function(plotDiv,data){
         const chartROC = plot.createLcChart(plotDiv)
         const rocSeries = chartROC.addLineSeries({color:"#80209C"})
         rocSeries.setData(plot.extractKeyAsValue(data,'roc'))
@@ -187,7 +186,7 @@ function plotFinanceROC(plot,dates){
 }
 
 function plotFinanceStoch(plot,dates){
-    plot.createSubPlot('stoch','finance','stoch',dates,function(plotDiv,data){
+    plot.createSubPlotLc('stoch','finance','stoch',dates,function(plotDiv,data){
         const chartSTOCH = plot.createLcChart(plotDiv)
         const slowkSeries = chartSTOCH.addLineSeries({color:"#FFEC00"})
         const slowdSeries = chartSTOCH.addLineSeries({color:"#FF9E00"})
@@ -197,7 +196,7 @@ function plotFinanceStoch(plot,dates){
 }
 
 function plotFinanceStochF(plot,dates){
-    plot.createSubPlot('stochf','finance','stochf',dates,function(plotDiv,data){
+    plot.createSubPlotLc('stochf','finance','stochf',dates,function(plotDiv,data){
         const chartSTOCHF = plot.createLcChart(plotDiv)
         const fastkSeries = chartSTOCHF.addLineSeries({color:"#00CDFF"})
         const fastdSeries = chartSTOCHF.addLineSeries({color:"#002B8E"})
@@ -207,7 +206,7 @@ function plotFinanceStochF(plot,dates){
 }
 
 function plotFinanceStochRSI(plot,dates){
-    plot.createSubPlot('stochrsi','finance','stochrsi',dates,function(plotDiv,data){
+    plot.createSubPlotLc('stochrsi','finance','stochrsi',dates,function(plotDiv,data){
         const chartSTOCHF = plot.createLcChart(plotDiv)
         const fastkSeries = chartSTOCHF.addLineSeries({color:"#00FF0C"})
         const fastdSeries = chartSTOCHF.addLineSeries({color:"#008E06"})
@@ -217,7 +216,7 @@ function plotFinanceStochRSI(plot,dates){
 }
 
 function plotFinanceTrix(plot,dates){
-    plot.createSubPlot('trix','finance','trix',dates,function(plotDiv,data){
+    plot.createSubPlotLc('trix','finance','trix',dates,function(plotDiv,data){
         const chartTRIX = plot.createLcChart(plotDiv)
         const trixSeries = chartTRIX.addLineSeries({color:"#FF0078"})
         trixSeries.setData(plot.extractKeyAsValue(data,'trix'))
@@ -225,7 +224,7 @@ function plotFinanceTrix(plot,dates){
 }
 
 function plotFinanceUltosc(plot,dates){
-    plot.createSubPlot('ultosc','finance','ultosc',dates,function(plotDiv,data){
+    plot.createSubPlotLc('ultosc','finance','ultosc',dates,function(plotDiv,data){
         const chartULTOSC = plot.createLcChart(plotDiv)
         const ultoscSeries = chartULTOSC.addLineSeries({color:"#FF0078"})
         ultoscSeries.setData(plot.extractKeyAsValue(data,'ultosc'))
@@ -233,7 +232,7 @@ function plotFinanceUltosc(plot,dates){
 }
 
 function plotFinanceWillr(plot,dates){
-    plot.createSubPlot('willr','finance','willr',dates,function(plotDiv,data){
+    plot.createSubPlotLc('willr','finance','willr',dates,function(plotDiv,data){
         const chartWILLR = plot.createLcChart(plotDiv)
         const willrSeries = chartWILLR.addLineSeries({color:"#7C4B00"})
         willrSeries.setData(plot.extractKeyAsValue(data,'willr'))
@@ -241,7 +240,7 @@ function plotFinanceWillr(plot,dates){
 }
 
 function plotFinanceIchimoku(plot,dates){
-    plot.createSubPlot('ichimoku','finance','ichimoku',dates,function(plotDiv,data){
+    plot.createSubPlotLc('ichimoku','finance','ichimoku',dates,function(plotDiv,data){
         const chartICHIMOKU = plot.createLcChart(plotDiv)
         const tenkanSeries = chartICHIMOKU.addLineSeries({color:"#FF0000"})
         const kijunSeries = chartICHIMOKU.addLineSeries({color:"#0C00FF"})
@@ -252,4 +251,23 @@ function plotFinanceIchimoku(plot,dates){
         ssaSeries.setData(plot.extractKeyAsValue(data,'ssa'))
         ssbSeries.setData(plot.extractKeyAsValue(data,'ssb'))
     })    
+}
+
+// Labels plots
+function setupPlotLabels(){
+    plot = new PlotImg('Labels plots',plotLabels)
+    plot.show()
+}
+
+function plotLabels(plot){
+    dates = plot.getDates()
+    plot.createSubPlot('labels','tbm',features,dates,function(){
+        
+    })
+}
+
+// Correlation plots
+function setupPlotCorrelation(){
+    plot = new PlotImg('Correlation plots',plotCorrelation)
+    plot.show()
 }
