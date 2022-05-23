c1eb996b812af7527873196b41b98da4f9e05fe5
Boris Slav <boris@slav.rocks>
2022-05-23 22:27:59 +0000

    rework labels plotter - front

--- apps/plotter/routers/labels.py
+++ apps/plotter/routers/labels.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from starlette.responses import FileResponse
 import matplotlib.pyplot as plt
-from tbmods.candles import Candles
+from tbmods.triple_barrier import TripleBarrier
+from tbmods.dataset import Dataset
+from tbmods.filters import Filters
 from tbmods.config import Config
-from tbmods.cache import Cache
 import seaborn as sns
 import pandas as pd
 import requests
@@ -20,43 +21,26 @@ sns.set(rc = {'figure.figsize':(11,4)})
 
 @router.get('/cusum/{timescale}/{from_date}/{to_date}/cusum.png')
 def graph_cusum(timescale,from_date,to_date):
-    # get close prices
-    r = requests.get('http://dataset/features/close/{}/{}/{}'.format(timescale,from_date,to_date))
-    if r.status_code != 200: raise HTTPException(status_code=500,detail="Unable to get dataset data")
-    close = Candles()
-    close.from_json(json.loads(r.json()))
-    # get cusum events
-    r = requests.get('http://dataset/labels/filters/cusum/{}/{}/{}'.format(timescale,from_date,to_date))
-    if r.status_code != 200: raise HTTPException(status_code=500,detail="Unable to get dataset data")
-    events = Candles()
-    events.from_json(json.loads(r.json()))
+    close = Dataset(timescale,from_date,to_date,'close').load()['close']
+    cusum = Filters(close).cusum_events(config['cusum_pct_threshold'])
     # graph
     fig, ax = plt.subplots()
-    sns.lineplot(x=close.candles.index,y='close',data=close.candles,color='green',label='price',alpha=0.3,ax=ax)
-    sns.scatterplot(x=events.candles.index,y=close.candles.loc[events.candles.index].close,hue='event',data=events.candles,ax=ax)
+    sns.lineplot(x=close.index,y=close.values,color='green',label='price',alpha=0.3,ax=ax)
+    sns.scatterplot(x=cusum.index,y=close.loc[cusum.index],hue='event',data=cusum,ax=ax)
     fig.savefig('/tmp/cusum.png')
     fig.clf()
     return FileResponse('/tmp/cusum.png',media_type="image/png")
 
 @router.get('/tbm/{timescale}/{from_date}/{to_date}/tbm.png')
 def graph_tbm(timescale,from_date,to_date):
-    # get close prices
-    r = requests.get('http://dataset/features/close/{}/{}/{}'.format(timescale,from_date,to_date))
-    if r.status_code != 200: raise HTTPException(status_code=500,detail="Unable to get dataset data")
-    close = Candles()
-    close.from_json(json.loads(r.json()))
-    # get tbm
-    r = requests.get('http://dataset/labels/tbm/{}/{}/{}'.format(timescale,from_date,to_date))
-    if r.status_code != 200: raise HTTPException(status_code=500,detail="Unable to get dataset data")
-    barriers = Candles()
-    barriers.from_json(json.loads(r.json()))
-    barriers.candles.vertical = pd.to_datetime(barriers.candles.vertical,unit='ms')
-    barriers.candles.first_touch = pd.to_datetime(barriers.candles.first_touch,unit='ms')
+    close = Dataset(timescale,from_date,to_date,'close').load()['close']
+    cusum = Filters(close).cusum_events(config['cusum_pct_threshold'])
+    barriers = TripleBarrier(close,cusum).barriers
     # graph
     fig, ax = plt.subplots()
-    sns.lineplot(x=close.candles.index,y='close',data=close.candles,color='green',label='price',alpha=0.3,ax=ax)
-    sns.scatterplot(x='first_touch',y='close_touch',data=barriers.candles,hue='side',palette=['r','k','g'],ax=ax)
-    for i,b in barriers.candles.iterrows():
+    sns.lineplot(x=close.index,y=close.values,color='green',label='price',alpha=0.3,ax=ax)
+    sns.scatterplot(x='first_touch',y='close_touch',data=barriers,hue='side',palette=['r','k','g'],ax=ax)
+    for i,b in barriers.iterrows():
         mid_price = (b.top+b.bot)/2
         ax.fill([i,b.vertical,b.vertical,i,i],[b.close,b.close,b.top,b.top,b.close],color='green',alpha=0.2)
         ax.fill([i,b.vertical,b.vertical,i,i],[b.bot,b.bot,b.close,b.close,b.bot],color='red',alpha=0.2)
@@ -66,14 +50,12 @@ def graph_tbm(timescale,from_date,to_date):
 
 @router.get('/balance/{timescale}/{from_date}/{to_date}/balance.png')
 def graph_balance(timescale,from_date,to_date):
-    # get tbm
-    r = requests.get('http://dataset/labels/tbm/{}/{}/{}'.format(timescale,from_date,to_date))
-    if r.status_code != 200: raise HTTPException(status_code=500,detail="Unable to get dataset data")
-    barriers = Candles()
-    barriers.from_json(json.loads(r.json()))
+    close = Dataset(timescale,from_date,to_date,'close').load()['close']
+    cusum = Filters(close).cusum_events(config['cusum_pct_threshold'])
+    barriers = TripleBarrier(close,cusum).barriers
     # graph
     fig, ax = plt.subplots()
-    sns.countplot(x='side',data=barriers.candles)
+    sns.countplot(x='side',data=barriers)
     fig.savefig('/tmp/balance.png')
     fig.clf()
     return FileResponse('/tmp/balance.png',media_type="image/png")
--- tbmods/filters/__init__.py
+++ tbmods/filters/__init__.py
@@ -10,9 +10,7 @@ class Filters:
     def cusum_events(self,threshold):
         threshold = threshold/100
         cusum_events = pd.DataFrame(columns=['event'])
-        candles = Candles()
-        candles.from_json(json.loads(self.close))
-        close_diff = candles.candles['close'].pct_change().dropna()
+        close_diff = self.close.pct_change().dropna()
         for date in close_diff.index:
             spos,sneg = 0,0
             spos,sneg = max(0,spos+close_diff.loc[date]),min(0,sneg+close_diff.loc[date])
