565cf2f0135eb061e7869d71a50b9176027be297
Boris Slav <boris@slav.rocks>
2022-05-25 10:40:52 +0000

    rework correlation plot - backend

--- apps/plotter/routers/correlation.py
+++ apps/plotter/routers/correlation.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, HTTPException
 from starlette.responses import FileResponse
 import matplotlib.pyplot as plt
-from tbmods.candles import Candles
+from tbmods.triple_barrier import TripleBarrier
+from tbmods.dataset import Dataset
+from tbmods.filters import Filters
 from tbmods.config import Config
-from tbmods.cache import Cache
 import seaborn as sns
 import pandas as pd
-import requests
 import json
 
 router = APIRouter(
@@ -16,27 +16,20 @@ router = APIRouter(
 
 config = Config()
 
-sns.set(rc = {'figure.figsize':(11,4)})
+sns.set(font_scale=0.5,rc = {'figure.figsize':(11,5)})
 
 @router.get('/features/{timescale}/{from_date}/{to_date}/features.png')
 def graph_balance(timescale,from_date,to_date):
-    # get features
-    r = requests.get('http://dataset/features/enabled_features/{}/{}/{}'.format(timescale,from_date,to_date))
-    if r.status_code != 200: raise HTTPException(status_code=500,detail='Unable to get dataset data')
-    else: features = json.loads(r.json())
-    candles = Candles()
-    candles.from_json(features)
-    # get labels
-    r = requests.get('http://dataset/labels/tbm/{}/{}/{}'.format(timescale,from_date,to_date))
-    if r.status_code != 200: raise HTTPException(status_code=500,detail="Unable to get dataset data")
-    barriers = Candles()
-    barriers.from_json(json.loads(r.json()))
+    config['features'] = json.loads(config['features'])
+    features_avail = ",".join(config['features']['sources']['qdb']+config['features']['sources']['talib'])
+    features = Dataset(timescale,from_date,to_date,features_avail).load()
+    cusum = Filters(features['close']).cusum_events(config['cusum_pct_threshold'])
+    labels = TripleBarrier(features['close'],cusum).barriers['side']
     # merge features and labels
-    candles.candles = candles.candles.reindex(candles.candles.index.intersection(barriers.candles.index))
-    candles.candles['side'] = barriers.candles.side.loc[candles.candles.index]
+    features = features.join(labels,how='inner')
     # graph
     fig,ax = plt.subplots()
-    sns.heatmap(candles.candles.corr(),ax=ax)
+    sns.heatmap(features.corr(),ax=ax)
     fig.savefig('/tmp/features_corr.png')
     fig.clf()
     return FileResponse('/tmp/features_corr.png',media_type="image/png")
