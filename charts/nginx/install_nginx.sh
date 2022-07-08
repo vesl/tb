083f5989be9f396574e517aae04760db3660a959
Boris Slav <boris@slav.rocks>
2022-07-08 19:06:37 +0200

    nginx installed from install_brs-k8s

--- charts/nginx/install_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for instance in $(ls values.*.yaml)
-do
-	release_name=$(echo $instance | awk -F. '{print $2}')
-	$(helm list | grep -q $release_name) && action=upgrade || action=install
-	helm $action $release_name -f $instance bitnami/nginx
-done
