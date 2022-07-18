789456906137a0b7c9f23917d8a9cf5bbfe08b5a
Boris Slav <boris@slav.rocks>
2022-07-18 11:19:15 +0000

    training tests

--- apps/trainer/routers/models.py
+++ apps/trainer/routers/models.py
@@ -1,5 +1,9 @@
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from tbmods.triple_barrier import TripleBarrier
 from fastapi import APIRouter, HTTPException
-from tbmods.candles import Candles
+from tbmods.filters import Filters
+from tbmods.dataset import Dataset
 from tbmods.config import Config
 from tbmods.cache import Cache
 from tbmods.log import Log
@@ -17,4 +21,14 @@ log = Log(config['app'])
 @router.get('/price/{timescale}/{from_date}/{to_date}')
 def price_model(timescale,from_date,to_date):
     # Get dataset
+    json_features = json.loads(config['features'])
+    all_features = json_features['sources']['qdb'] + json_features['sources']['talib']
+    dataset = Dataset(timescale,from_date,to_date,all_features).load()
+    cusum = Filters(dataset.close).cusum_events(config['cusum_pct_threshold'])
+    Y = TripleBarrier(dataset.close,cusum).barriers.side
+    X = dataset.loc[Y.index]
+    clf = RandomForestClassifier(random_state=42)
+    scores = cross_val_score(clf, X, Y, cv=5)
+    print(scores)
+    print(Y.shape)
     return 1
