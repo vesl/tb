ffc7622791020fa5652ca87b9a9bb0d574828418
Boris Slav <boris@slav.rocks>
2022-07-20 11:58:45 +0000

    tests trainer - grid search

--- apps/trainer/routers/models.py
+++ apps/trainer/routers/models.py
@@ -25,28 +25,40 @@ log = Log(config['app'])
 
 @router.get('/price/{timescale}/{from_date}/{to_date}')
 def price_model(timescale,from_date,to_date):
-    # Get dataset
+    
+    # FEATURES
     json_features = json.loads(config['features'])
     all_features = json_features['sources']['qdb'] + json_features['sources']['talib']
     dataset = Dataset(timescale,from_date,to_date,all_features).load()
-    nb_prev = 1
-    for n in range(1,nb_prev+1):
+    for n in range(1,4):
         for feature in dataset.columns:
-            dataset['{}-{}'.format(feature,n)] = dataset[feature].shift(n)
+            n_feature = pd.DataFrame(dataset[feature].shift(n),columns=['{}-{}'.format(feature,n)])
+            dataset = pd.concat([dataset,],axis=1,join='inner')
+    dataset.dropna(inplace=True)
+    all_features = dataset.columns
     cusum = Filters(dataset.close).cusum_events(config['cusum_pct_threshold'])
-    Y = TripleBarrier(dataset.close,cusum).barriers.side
-    X = dataset.loc[Y.index]
-    scaler = MinMaxScaler()
-    X = scaler.fit_transform(X)
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
-    clf = RandomForestClassifier(random_state=42)
-    clf.fit(X_train,Y_train)
-    Y_pred = clf.predict(X_test)
-    f1 = f1_score(Y_test,Y_pred,average=None)
-    print(f1)
-    cm = confusion_matrix(Y_test,Y_pred)
-    print(cm)
-    importances = clf.feature_importances_
-    fi = pd.Series(importances, index=dataset.columns).sort_values(ascending=False)
-    print(fi)
+    
+    # RECURSIVE TRAIN
+    while len(all_features) >= 5:
+        print("===> TRAIN")
+        print(all_features)
+        dataset = dataset.loc[:,all_features]
+        print(dataset.head())
+        Y = TripleBarrier(dataset.close,cusum).barriers.side
+        X = dataset.loc[Y.index]
+        scaler = MinMaxScaler()
+        X = scaler.fit_transform(X)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
+        clf = RandomForestClassifier(random_state=42)
+        clf.fit(X_train,Y_train)
+        Y_pred = clf.predict(X_test)
+        f1 = f1_score(Y_test,Y_pred,average=None)
+        print(f1)
+        cm = confusion_matrix(Y_test,Y_pred)
+        print(cm)
+        importances = clf.feature_importances_
+        fi = pd.Series(importances, index=dataset.columns).sort_values(ascending=False)
+        print(fi)
+        all_features = fi.iloc[:-6].index.tolist()
+        if not 'close' in all_features: all_features.append('close')
     return 1
