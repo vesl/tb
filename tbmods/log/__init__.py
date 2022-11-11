b4567f8168d9115f4fb4813cdd032fbddb9d7f34
Boris Slav <boris@slav.rocks>
2022-11-11 11:53:19 +0000

    stop execution when log.error called

--- tbmods/log/__init__.py
+++ tbmods/log/__init__.py
@@ -2,12 +2,16 @@ class Log:
     
     def __init__(self,app):
         self.app = app
-        
+
+    # something goes wrong and execution should stop
     def error(self,msg):
         print("ERROR:     {} - {}".format(self.app,msg))
-        
+        raise Exception('Execution aborted')
+    
+    # something goes wrong and execution should continue
     def warning(self,msg):
         print("WARNING:     {} - {}".format(self.app,msg))
         
+    # something interesting happened
     def info(self,msg):
         print("INFO:     {} - {}".format(self.app,msg))
