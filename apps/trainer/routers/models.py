1579f3e17a67a7c13e69bbef3bc6182f0483184e
Boris Slav <boris@slav.rocks>
2022-12-19 12:29:10 +0000

    implement meta for tech model

--- apps/trainer/routers/models.py
+++ apps/trainer/routers/models.py
@@ -23,48 +23,4 @@ def grid_search_tech(period,start,end):
     model = ModelTech(period,start,end,features_list)
     model.clf_init(json.loads(config['tech_clf_config']))
     model.fit()
-    model.save()
-    
-    
-"""
-    # Generate features list with lag
-    for feature in features_list.copy():
-        for lag in range(1,config['tech_grid_search_lag']+1):
-            features_list.append('{}-{}'.format(feature,lag))
-    # Generate dataset
-    dataset = DatasetTech(period,start,end,features_list)
-
-    # GRID SEARCH
-    results = []
-    while len(features_list) >= 5:
-        result = {}
-        dataset.features = dataset.features.loc[:,features_list]
-        y = dataset.labels
-        X = dataset.features
-        result['features'] = features_list
-        result['cv_scores'] = clf.cv()
-        clf.fit()
-        result['f1_score'] = clf.f1_score()
-        result['cm'] = clf.confusion_matrix()
-        result['fi'] = clf.feature_importances()
-        features_list =  result['fi'].iloc[:-6].index.tolist()
-        results.append(result)
-    
-    # GET BEST RESULT
-    best_result = {}
-    for result in results:
-        print(result['f1_score'])
-        if any(f1 < 0.5 for f1 in result['f1_score']): continue
-        avg = sum(result['f1_score'])/3
-        if not 'avg' in best_result or best_result['avg'] < avg:
-            best_result = result
-            best_result['avg'] = avg
-    print("BEST RESULT")
-    print("FEATURES : {}".format(list(best_result['features'])))
-    print("CV_SCORES : {}".format(list(best_result['cv_scores'])))
-    print("F1_SCORE : {}".format(list(best_result['f1_score'])))
-    print("CONFUSION MATRIX :")
-    print(best_result['cm'])
-    print("FEATURES IMPORTANCE :")
-    print(best_result['fi'].sort_values(ascending=False))
-"""
+    print(model.meta)
--- tbmods/models/tech.py
+++ tbmods/models/tech.py
@@ -6,14 +6,17 @@ from sklearn.metrics import confusion_matrix
 from tbmods.dataset.tech import DatasetTech
 from sklearn.feature_selection import chi2
 from sklearn.metrics import f1_score
+from datetime import datetime
 import pandas as pd
 
 class ModelTech:
     
     def __init__(self,period,start,end,features_list):
+        self.name = "tech-{}".format(datetime.now().strftime("%Y%m%d-%H%M%S"))
         self.scaler = MinMaxScaler()
         self.features_list = features_list
         self.dataset = DatasetTech(period,start,end,self.features_list)
+        self.meta = {"name": self.name, "features": self.features_list}
         self.X = self.scaler.fit_transform(self.dataset.features)
         self.y = self.dataset.labels
     
@@ -26,6 +29,7 @@ class ModelTech:
             class_weight=config['class_weight'],
             random_state=int(config['random_state']),
         )
+        self.meta.update({"clf_config":config})
 
     def chi2_test(self):
         return pd.DataFrame([chi2(self.X,self.y)[0]],columns=self.features_list)
@@ -34,9 +38,17 @@ class ModelTech:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2)
         self.clf.fit(self.X_train, self.y_train)
         self.y_pred = self.clf.predict(self.X_test)
+        self.meta.update {
+            "score": {
+                "cross_val_score": list(self.cross_val_score()),
+                "f1_score": list(self.f1_score()),
+                "confusion_matrix": list(self.confusion_matrix()),
+                "feature_importances": self.feature_importances().to_json(),
+            }
+        }
 
-    def cv(self):
-        return cross_val_score(self.model,self.X,self.y,cv=5)
+    def cross_val_score(self):
+        return cross_val_score(self.clf,self.X,self.y,cv=5)
 
     def f1_score(self):
         return f1_score(self.y_test,self.y_pred,average=None)
@@ -45,4 +57,5 @@ class ModelTech:
         return confusion_matrix(self.y_test,self.y_pred)
         
     def feature_importances(self):
-        return pd.Series(self.model.feature_importances_,index=self.features_list)
+        return pd.Series(self.clf.feature_importances_,index=self.features_list)
+        
