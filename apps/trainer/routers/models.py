02d8f82682c6c02f97d3422fb3eca6f046003fd9
Boris Slav <boris@slav.rocks>
2022-12-25 08:50:26 +0000

    trainer add cache mgmt

--- apps/trainer/routers/models.py
+++ apps/trainer/routers/models.py
@@ -17,10 +17,23 @@ router = APIRouter(
 config = Config()
 log = Log(config['app'])
 
-@router.get('/tech/train/{features}/{period}/{start}/{end}')
-def grid_search_tech(period,start,end):
+@router.get('/tech/train/{period}/{start}/{end}')
+def tech_train(period,start,end):
+    
+    cache = Cache(config['app'])
+    cache.data['/models/tech'] = {"run":True}
+    cache.write()
+    
     features_list = config['tech_features_selected'].split(',')
-    model = ModelTech(period,start,end,features_list)
-    model.clf_init(json.loads(config['tech_clf_config']))
-    model.fit()
-    model.save_meta()
+    tech_model = ModelTech(period,start,end,features_list)
+    tech_model.clf_init(json.loads(config['tech_clf_config']))
+    tech_model.fit()
+    tech_model.save_meta()
+    
+    cache.data['/models/tech'] = {"run":False}
+    cache.write()
+    
+@router.get('/tech/run')
+def tech_run():
+    cache = Cache(config['app'])
+    return cache.data['/models/tech']
