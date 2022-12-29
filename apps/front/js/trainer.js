d325011e3148261c20eefa407dbe1ea8c74ad7bc
Boris Slav <boris@slav.rocks>
2022-12-29 12:45:21 +0000

    front - setup model tech training loop

--- apps/front/js/trainer.js
+++ apps/front/js/trainer.js
@@ -1,3 +1,26 @@
+function trainerCheckRun(next){
+    $.get('/api/trainer/models/tech/check_run',(data)=>{
+        if (data["run"] == true) {
+            contentClearContent()
+            contentHTML('Trainer is already running ...')
+            contentPre(JSON.stringify(data,undefined, 2))
+            setTimeout(()=>{trainerModelTech()},10000)
+        }
+        else next()
+    })
+}
+
+function trainerTrainModelTech(){
+    var dpValues = getDpValues()
+    if (dpValues) $.get('/api/trainer/models/tech/train/'+dpValues.period+'/'+dpValues.start+'/'+dpValues.end)
+    setTimeout(()=>{trainerModelTech()},2000)
+}
+
 function trainerModelTech(){
     contentTitle('Model - Tech')
+    contentClearContent()
+    trainerCheckRun(()=>{
+        contentDatePicker()
+        contentButton('Train',()=>{trainerTrainModelTech()}) 
+    })
 }
