eb252c3056247a95bb254c183bfa8c50df6792dc
Boris Slav <boris@slav.rocks>
2022-12-30 12:21:31 +0000

    tech model - split methods and add update status

--- tbmods/models/tech.py
+++ tbmods/models/tech.py
@@ -7,18 +7,45 @@ from tbmods.dataset.tech import DatasetTech
 from sklearn.feature_selection import chi2
 from sklearn.metrics import f1_score
 from tbmods.mongodb import MongoDB
+from tbmods.config import Config
+from tbmods.cache import Cache
 from datetime import datetime
 import pandas as pd
+
+config = Config()
 
 class ModelTech:
     
     def __init__(self,period,start,end,features_list):
+        self.period = period
+        self.start = start
+        self.end = end
+        self.features_list = features_list
         self.mongodb = MongoDB()
         self.name = "tech-{}".format(datetime.now().strftime("%Y%m%d-%H%M%S"))
+        self.init_meta()
+        
+    def init_meta(self):
+        self.meta = {"name": self.name}
+        
+    def update_status(self,update):
+        cache = Cache(config['app'])
+        if type(update) is bool: self.status = {}
+        else: self.status.update(update)
+        cache.data["models/tech/status"] = self.status
+        cache.write()
+        
+    def load_dataset(self):
+        self.dataset = DatasetTech(self.period,self.start,self.end,self.features_list)
+        self.meta.update({
+            "period":self.period,
+            "start":self.start,
+            "end":self.end,
+            "n_samples":len(self.dataset.labels)
+        })
+        
+    def scale(self):
         self.scaler = MinMaxScaler()
-        self.features_list = features_list
-        self.dataset = DatasetTech(period,start,end,self.features_list)
-        self.meta = {"name": self.name, "features": self.features_list}
         self.X = self.scaler.fit_transform(self.dataset.features)
         self.y = self.dataset.labels
     
@@ -33,10 +60,8 @@ class ModelTech:
         )
         self.meta.update({"clf_config":config})
 
-    def chi2_test(self):
-        return pd.DataFrame([chi2(self.X,self.y)[0]],columns=self.features_list)
-
     def fit(self):
+        self.scale()
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2)
         self.clf.fit(self.X_train, self.y_train)
         self.y_pred = self.clf.predict(self.X_test)
@@ -60,6 +85,10 @@ class ModelTech:
         
     def feature_importances(self):
         return pd.Series(self.clf.feature_importances_,index=self.features_list)
+        
+    def chi2_test(self):
+        self.scale()
+        return pd.DataFrame([chi2(self.X,self.y)[0]],columns=self.features_list)
         
     def save_meta(self):
         self.mongodb.insert('models','tech',self.meta)
