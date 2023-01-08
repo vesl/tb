d384a0c8902a9df056249dccbb8c24e4067fa178
Boris Slav <boris@slav.rocks>
2023-01-08 17:22:38 +0000

    tbmods - add mongodb

--- tbmods/mongodb/__init__.py
+++ tbmods/mongodb/__init__.py
@@ -10,3 +10,6 @@ class MongoDB:
 
     def insert(self,db,collection,doc):
         self.client[db][collection].insert_one(doc)
+        
+    def find(self,db,collection,search={}):
+        return self.client[db][collection].find(search,{"_id":0})
