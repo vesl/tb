fff2a7a01baed76d8e1f6c50fe591ad448f2ed6e
Boris Slav <boris@slav.rocks>
2023-02-06 11:37:33 +0000

    plotter - add market endpoints

---
+++ apps/plotter/routers/market.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException
+from tbmods.mongodb import MongoDB
+from tbmods.config import Config
+from tbmods.log import Log
+import json
+
+router = APIRouter(
+    prefix="/market",
+    tags=["market"],
+)
+
+config = Config()
+log = Log(config['app'])
+mongodb = MongoDB()
+
+@router.get('/backtest/results/list')
+def get_backtest_results_list():
+    backtest_results_list = {}
+    for backtest_result in mongodb.find('market','backtest'):
+        backtest_results_list[backtest_result["name"]] = backtest_result
+    return backtest_results_list
