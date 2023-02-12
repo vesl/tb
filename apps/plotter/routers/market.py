ca1238630838d0f62b68c96e71d2ec4d6b81e627
Boris Slav <boris@slav.rocks>
2023-02-12 20:17:57 +0000

    plotter - add paper results

--- apps/plotter/routers/market.py
+++ apps/plotter/routers/market.py
@@ -19,3 +19,7 @@ def get_backtest_results_list():
     for backtest_result in mongodb.find('market','backtest'):
         backtest_results_list[backtest_result["name"]] = backtest_result
     return backtest_results_list
+    
+@router.get('/paper/results')
+def get_paper_results():
+    return mongodb.find_one('market','paper')
