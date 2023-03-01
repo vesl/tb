614d9219b110629f0ecc44c0593cd1642a54ac45
Boris Slav <boris@slav.rocks>
2023-03-01 13:38:39 +0000

    trader market - remove paper function

--- apps/trader/routers/market.py
+++ apps/trader/routers/market.py
@@ -57,41 +57,7 @@ def get_backtest(symbol,period,start,end):
 
 @router.get('/paper')
 def get_paper():
-    timer = datetime.now()
-    # init market
-    market_paper = MarketPaper("BUSD",1000,"BTC",0)
-    market_paper.name = 'paper'
-    market_paper.load_meta()
-    # prepare time
-    start = config['tech_data_start']
-    period = config['tech_data_period']
-    end = pd.to_datetime('today')+pd.Timedelta(days=1)
-    current_time = pd.to_datetime(start,utc=True)
-    end_time = pd.to_datetime(end,utc=True)
-    # get last_time and next_time
-    cache = Cache(config['app'])
-    last_time = pd.to_datetime(cache.data['paper/status']["last_time"])
-    current_time = last_time + pd.Timedelta(hours=1)
-    next_time = current_time + pd.Timedelta(hours=1)
-    # prepare data
-    dataset,scaler,price,events = prepare_data(period,start,end)
-    if next_time in dataset.full_features.index:
-        market_paper.set_time(next_time)
-        market_paper.set_price(dataset.klines.df.close[next_time])
-        print(events.tail())
-        log.info("Last time {}".format(last_time))
-        log.info("Current time {}".format(current_time))
-        log.info("Next time {}".format(next_time))
-        if current_time in events.index:
-            log.info("Event detected !")
-            X = scaler.transform([dataset.full_features.loc[current_time]])
-            market_paper.trigger(X)
-        else:
-            log.info("No event detected last one {}".format(events.index[-1]))
-            market_paper.trigger()
-        market_paper.update_status({"last_time":str(current_time)})
-        market_paper.save_meta()
-    log.info("Duration : {}".format(datetime.now()-timer))
+    pass
 
 @router.get('/live')
 def get_live():
