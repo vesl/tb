a427cd3f5cf96b9ef2ea3fc097a36812c9b0e704
Boris Slav <boris@slav.rocks>
2023-04-25 11:42:17 +0000

    dashboard - add globalFunctions

--- apps/dashboard/src/main.js
+++ apps/dashboard/src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '@/router'
+import { stringFunctions } from '@/globalFunctions.js'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$stringFunctions = stringFunctions
+app.use(router)
+app.mount('#app')
