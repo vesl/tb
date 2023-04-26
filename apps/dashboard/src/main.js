bef709f4f9ecabf242dedc385a32782aac1b9fc6
Boris Slav <boris@slav.rocks>
2023-04-26 11:15:14 +0000

    dashboard - main.js fix " => '

--- apps/dashboard/src/main.js
+++ apps/dashboard/src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from '@/router'
 import { stringFunctions } from '@/globalFunctions.js'
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 const app = createApp(App)
 app.config.globalProperties.$stringFunctions = stringFunctions
