4a7d0f895d6759c169b306139a3321940e3f53da
Boris Slav <boris@slav.rocks>
2023-04-29 14:22:31 +0000

    scrapper - fix cors

--- apps/scrapper/main.py
+++ apps/scrapper/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from scrapper.routers import klines
+from fastapi.middleware.cors import CORSMiddleware
 
 ## init api
 app = FastAPI(
     title="Scrapper",
     description="Scrap financial data from binance and store it in questdb",
     version="1.1.0"
+)
+## cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 ## init routers
 app.include_router(klines.router)
