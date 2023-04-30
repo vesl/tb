754d44a7bbfd81f39539a4a5446d9c79d91a2fd4
Boris Slav <boris@slav.rocks>
2023-04-30 08:36:49 +0000

    dashboard - SrapperKlines pull last klines info

--- apps/dashboard/src/components/organisms/ScrapperKlines.vue
+++ apps/dashboard/src/components/organisms/ScrapperKlines.vue
@@ -1,10 +1,23 @@
 <template>
-    OK
+    <div class="row">
+        <ContentInfo :class="'col-5'" :title="'Last Live Kline'">
+            {{ lastLiveKline }}
+        </ContentInfo>
+        <ContentInfo :class="'col-5'" :title="'Last Live Kline'">
+            {{ lastHistoricalKline }}
+        </ContentInfo>
+    </div>
 </template>
 
 <script>
+import ContentInfo from '../molecules/ContentInfo.vue'
+import axios from 'axios';
+
 export default {
     name: 'scrapper-klines',
+    components: {
+      ContentInfo
+    },
     props: {
         app: {
             type: String,
@@ -13,7 +26,25 @@ export default {
         view: {
             type: String,
             required: true
+        },
+        symbol: {
+            type: String,
+            required: true
         }
+    },
+    data(){
+      return {
+          lastLiveKline: null,
+          lastHistoricalKline: null
+      }  
+    },
+    mounted(){
+        axios.get('http://scrapper'+this.$store.state.apis_domain+'/klines/get/last/live/'+this.symbol)
+            .then(response => {this.lastLiveKline = JSON.parse(response.data)})
+            .catch(error => {this.lastLiveKline = error;});
+        axios.get('http://scrapper'+this.$store.state.apis_domain+'/klines/get/last/historical/'+this.symbol)
+            .then(response => {this.lastHistoricalKline = JSON.parse(response.data)})
+            .catch(error => {this.lastHistoricalKline = error;});
     }
 }
 </script>
