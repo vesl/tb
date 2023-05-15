8daaeae749b53dbc78103ac50d0738e09d3c75ee
Boris Slav <boris@slav.rocks>
2023-05-15 11:47:11 +0000

    dashboard - globalFunctions edit firstLetterUpper to apply it on all words

--- apps/dashboard/src/globalFunctions.js
+++ apps/dashboard/src/globalFunctions.js
@@ -1,5 +1,9 @@
 export const stringFunctions = {
     firstLetterUpper: (string) => {
-        return string.charAt(0).toUpperCase() + string.slice(1)
+        const words = string.split(" ");
+        for (let i = 0; i < words.length; i++) {
+            words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+        }
+        return words.join(" ");
     }
 }
