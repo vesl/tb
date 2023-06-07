6cf70541832088b5c2d664ec3630c32129007cae
Boris Slav <boris@slav.rocks>
2023-06-07 17:38:09 +0000

    trainer - add get maps,names,types, dataset

--- apps/trainer/routers/datasets.py
+++ apps/trainer/routers/datasets.py
@@ -18,3 +18,25 @@ def get_maps():
     maps = [doc for doc in mongodb.find("TB","datasets_maps")]
     mongodb.close()
     return maps
+
+@router.get('/get/maps/{_type}')
+def get_maps_by_type(_type):
+    mongodb = MongoDB()
+    maps = [doc for doc in mongodb.find("TB","datasets_maps",{"type":_type})]
+    mongodb.close()
+    return maps
+
+@router.get('/get/names/{_type}')
+def get_names_by_type(_type):
+    mongodb = MongoDB()
+    maps = [doc['name'] for doc in mongodb.find("TB","datasets_maps",{"type":_type})]
+    mongodb.close()
+    return maps
+
+@router.get('/get/types')
+def get_types():
+    mongodb = MongoDB()
+    types = []
+    for doc in mongodb.groupby("TB","datasets_maps","type"):
+        types.append(doc["_id"])
+    return types
