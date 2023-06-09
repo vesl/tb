ca1e088499b4e69e12abe174646ffc9e42e5b0c5
Boris Slav <boris@slav.rocks>
2023-06-09 12:03:03 +0000

    trainer - add get_dataset_features_maps_by_name endpoint

--- apps/trainer/routers/datasets.py
+++ apps/trainer/routers/datasets.py
@@ -1,3 +1,4 @@
+from trainer.routers.features import get_features_map_by_name
 from fastapi import APIRouter, HTTPException
 #from tbmods.dataset import Dataset
 from tbmods.mongodb import MongoDB
@@ -13,30 +14,41 @@ config = Config()
 log = Log(config['app'])
 
 @router.get('/get/maps')
-def get_maps():
+def get_dataset_maps():
     mongodb = MongoDB()
     maps = [doc for doc in mongodb.find("TB","datasets_maps")]
     mongodb.close()
     return maps
 
 @router.get('/get/maps/{_type}')
-def get_maps_by_type(_type):
+def get_dataset_maps_by_type(_type):
     mongodb = MongoDB()
     maps = [doc for doc in mongodb.find("TB","datasets_maps",{"type":_type})]
     mongodb.close()
     return maps
 
 @router.get('/get/names/{_type}')
-def get_names_by_type(_type):
+def get_dataset_names_by_type(_type):
     mongodb = MongoDB()
     maps = [doc['name'] for doc in mongodb.find("TB","datasets_maps",{"type":_type})]
     mongodb.close()
     return maps
 
 @router.get('/get/types')
-def get_types():
+def get_dataset_types():
     mongodb = MongoDB()
     types = []
     for doc in mongodb.groupby("TB","datasets_maps","type"):
         types.append(doc["_id"])
+    mongodb.close()
     return types
+    
+@router.get('/get/features_maps/{name}')
+def get_dataset_features_maps_by_name(name):
+    mongodb = MongoDB()
+    features_maps_names = [doc['features_maps'] for doc in mongodb.find("TB","datasets_maps",{"name":name})][0]
+    mongodb.close()
+    features_maps = []
+    for features_map_name in features_maps_names:
+        features_maps.append(get_features_map_by_name(features_map_name))
+    return features_maps
--- apps/trainer/routers/features.py
+++ apps/trainer/routers/features.py
@@ -14,14 +14,21 @@ config = Config()
 log = Log(config['app'])
 
 @router.get('/get/maps')
-def get_maps():
+def get_features_maps():
     mongodb = MongoDB()
     maps = [doc for doc in mongodb.find("TB","features_maps")]
     mongodb.close()
     return maps
-    
+
+@router.get('/get/map/{name}')
+def get_features_map_by_name(name):
+    mongodb = MongoDB()
+    features_map = mongodb.find("TB","features_maps",{"name":name})[0]
+    mongodb.close()
+    return features_map
+
 @router.post('/plot/{start}/{end}/{symbol}/{period}')
-def plot_feature(features_map: FeaturesMap,start,end,symbol,period):
+def plot_features_maps(features_map: FeaturesMap,start,end,symbol,period):
     features = Features(features_map.dict(),start,end,symbol,period)
     features.df["time"] = features.df.index.astype(int)/1000000000 
     return features.df.to_json(orient='records')
