48c795bcb66a58967b46446330e4a51f311e81d1
Boris Slav <boris@slav.rocks>
2023-06-11 17:49:43 +0000

    dashboard - change KlinesDetail component to KlinesTable component

---
+++ apps/dashboard/src/components/molecules/KlinesTable.vue
@@ -0,0 +1,58 @@
+<template>
+    <PrimeCard>
+        <template #title>Klines Table - {{period}}</template>
+        <template #content>
+            <PrimeDataTable v-if="firstLastKlines.length" :value="firstLastKlines" showGridlines>
+                <PrimeColumn v-for="column in columns" :key="column" :field="column" :header="column" />
+            </PrimeDataTable>
+        </template>
+    </PrimeCard>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+    name: 'klines-table',
+    props: ['period','symbol'],
+    data(){
+        return {
+            firstLastKlines: [],
+            klinesCount: 0,
+            columns: [],
+        }
+    },
+    methods: {
+        formatDate(klines){
+            return klines.map( kline => {
+                Object.keys(kline).forEach((key)=>{
+                    if(key == 'open_time' || key == 'close_time') kline[key] = new Date(kline[key]).toUTCString()
+                })
+                return kline
+            })
+        },
+        getFirstLastKlines(){
+            axios.get('http://scrapper'+this.$store.state.apis_domain+'/klines/get/first/'+this.period+'/'+this.symbol)
+                .then(response => {
+                    
+                    let firstKline = this.formatDate(JSON.parse(response.data))
+                    this.firstLastKlines = firstKline
+
+                    axios.get('http://scrapper'+this.$store.state.apis_domain+'/klines/get/last/'+this.period+'/'+this.symbol)
+                        .then(response => {
+                            
+                            let lastKline = this.formatDate(JSON.parse(response.data))
+                            this.firstLastKlines = this.firstLastKlines.concat(lastKline)
+                            this.columns = Object.keys(this.firstLastKlines[0])
+                        })
+                        .catch(error => {this.$toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 })})
+                })
+                .catch(error => {this.$toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 })})
+        },
+    },
+    mounted(){
+        this.getFirstLastKlines()
+    }
+    
+}
+</script>
--- apps/dashboard/src/views/ScrapperKlines.vue
+++ apps/dashboard/src/views/ScrapperKlines.vue
@@ -2,23 +2,7 @@
 
     <div class="p-3">
 
-        <PrimeCard>
-            <template #title>First Live Kline</template>
-            <template #content>
-                <KlinesDetail v-if="firstLiveKline" :klines="firstLiveKline" />
-                <PrimeProgressSpinner v-else />
-            </template>
-        </PrimeCard>
-        
-        <PrimeDivider />
-        
-        <PrimeCard>
-            <template #title>Last Live Kline</template>
-            <template #content>
-                <KlinesDetail v-if="lastLiveKline" :klines="lastLiveKline" />
-                <PrimeProgressSpinner v-else />
-            </template>
-        </PrimeCard>
+        <KlinesTable v-for="period in periods" :key="period" :period="period" :symbol="symbol" />
         
         <PrimeDivider />
         
@@ -29,27 +13,7 @@
                 <PrimeProgressSpinner v-else />
             </template>
         </PrimeCard>
-        
-        <PrimeDivider />
-        
-        <PrimeCard>
-            <template #title>First Historical Kline</template>
-            <template #content>
-                <KlinesDetail v-if="firstHistoricalKline" :klines="firstHistoricalKline" />
-                <PrimeProgressSpinner v-else />
-            </template>
-        </PrimeCard>
 
-        <PrimeDivider />
-
-        <PrimeCard>
-            <template #title>Last Historical Kline</template>
-            <template #content>
-                <KlinesDetail v-if="lastHistoricalKline" :klines="lastHistoricalKline" />
-                <PrimeProgressSpinner v-else />
-            </template>
-        </PrimeCard>
-        
         <PrimeDivider />
         
         <PrimeCard>
@@ -65,14 +29,14 @@
 </template>
 
 <script>
-import KlinesDetail from '@/components/molecules/KlinesDetail.vue'
+import KlinesTable from '@/components/molecules/KlinesTable.vue'
 import LWChart from '@/components/atoms/LWChart.vue'
 import axios from 'axios'
 
 export default {
     name: 'scrapper-klines',
     components: {
-      KlinesDetail,
+      KlinesTable,
       LWChart
     },
     props: {
@@ -93,38 +57,18 @@ export default {
       return {
           liveKlines: null,
           historicalKlines: null,
-          firstLiveKline: null,
-          firstHistoricalKline: null,
-          lastLiveKline: null,
-          lastHistoricalKline: null,
+          periods: ['live','historical']
       }  
     },
     methods: {
         getApiData(){
             this.liveKlines = null
             this.historicalKlines = null
-            this.firstLiveKline = null
-            this.firstHistoricalKline = null
-            this.lastLiveKline = null
-            this.lastHistoricalKline = null
             axios.get('http://scrapper'+this.$store.state.apis_domain+'/klines/get/live/'+this.symbol)
                 .then(response => {this.liveKlines = JSON.parse(response.data)})
                 .catch(error => {this.$toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 })})
             axios.get('http://scrapper'+this.$store.state.apis_domain+'/klines/get/historical/'+this.symbol)
                 .then(response => {this.historicalKlines = JSON.parse(response.data)})
-                .catch(error => {this.$toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 })})
-            axios.get('http://scrapper'+this.$store.state.apis_domain+'/klines/get/last/live/'+this.symbol)
-                .then(response => {this.lastLiveKline = JSON.parse(response.data)})
-                .catch(error => {this.$toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 })})
-            axios.get('http://scrapper'+this.$store.state.apis_domain+'/klines/get/last/historical/'+this.symbol)
-                .then(response => {this.lastHistoricalKline = JSON.parse(response.data)})
-                .catch(error => {this.$toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 })})
-            axios.get('http://scrapper'+this.$store.state.apis_domain+'/klines/get/first/live/'+this.symbol)
-                .then(response => {this.firstLiveKline = JSON.parse(response.data)})
-                .catch(error => {this.$toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 })})
-            axios.get('http://scrapper'+this.$store.state.apis_domain+'/klines/get/first/historical/'+this.symbol)
-                .then(response => {this.firstHistoricalKline = JSON.parse(response.data)})
-                .catch(error => {this.$toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 })})
         }
     },
     watch: {
