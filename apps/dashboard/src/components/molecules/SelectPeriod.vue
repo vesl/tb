1db84c9cafe1e7e7db7019a120f4c2cba304c3d4
Boris Slav <boris@slav.rocks>
2023-06-14 09:23:30 +0000

    dashboard - add SelectPeriod component

---
+++ apps/dashboard/src/components/molecules/SelectPeriod.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex-auto">
+        <label class="text-sm block mb-2">Period</label>
+        <PrimeDropdown v-model="selectedPeriod" :options="$store.state.periods" placeholder="Select a period" class="mr-2" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['period'],
+    data(){
+        return {
+            selectedPeriod: this.period
+        }
+    },
+    watch: {
+        selectedPeriod(period) {
+            this.$emit('update:period', period)
+            this.$cookies.set('selectedPeriod', period)
+        }
+    }
+}
+</script>
