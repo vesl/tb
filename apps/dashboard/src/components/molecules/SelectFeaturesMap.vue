bea47c958543f04ec02f799e1f8136e4d4ec4a7b
Boris Slav <boris@slav.rocks>
2023-06-14 13:26:50 +0000

    dashboard - create SelectFeaturesMap component

---
+++ apps/dashboard/src/components/molecules/SelectFeaturesMap.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex-auto">
+        <label class="text-sm block mb-2">Features Maps</label>
+        <PrimeDropdown v-if="featuresMaps" v-model="selectedFeaturesMap" :options="featuresMaps" placeholder="Select a features map" class="mr-2" />
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+    props: ['featuresMap','dataset'],
+    data(){
+      return {
+          selectedFeaturesMap: null,
+          featuresMaps: null
+      }  
+    },
+    watch: {
+        selectedFeaturesMap(featuresMap) {
+            this.$emit('update:featuresMap', featuresMap)
+            this.$cookies.set('selectedFeaturesMap', featuresMap)
+        }
+    },
+    methods: {
+        getFeaturesMaps(){
+            axios.get('http://trainer'+this.$store.state.apis_domain+'/datasets/get/features_maps/'+this.dataset)
+                .then(response => {this.featuresMaps = response.data.map(featuresMap => featuresMap.name)})
+                .catch(error => {this.$toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 })})
+        }
+    },
+    mounted(){
+        this.getFeaturesMaps()
+    }
+}
+</script>
