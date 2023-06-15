85c688776ce0d1195ce866c23349a8cc721a8af0
Boris Slav <boris@slav.rocks>
2023-06-15 17:25:00 +0000

    tbmods - create dataset class and implement load_features

---
+++ tbmods/dataset.py
@@ -0,0 +1,47 @@
+from tbmods.features import Features
+from tbmods.mongodb import MongoDB
+from tbmods.config import Config
+from tbmods.log import Log
+import pandas as pd
+
+config = Config()
+log = Log(config['app'])
+
+class Dataset:
+    
+    def __init__(self,name,start,end,symbol,period):
+        self.name = name
+        self.get_map()
+        self.start = start
+        self.end = end
+        self.symbol = symbol
+        self.period = period
+        self.load_features()
+
+    def get_map(self):
+        mongodb = MongoDB()
+        try:
+            self._map = mongodb.find('TB','datasets_maps',{'name':{'$eq':self.name}})[0]
+        except IndexError:
+            log.error("Can't find dataset named {} in mongo".format(self.name))
+        mongodb.close()
+        
+    def get_features_map(self,features_map_name):
+        mongodb = MongoDB()
+        try:
+            return mongodb.find('TB','features_maps',{'name':{'$eq':features_map_name}})[0]
+        except IndexError:
+            log.error("Can't find feature map named {} in mongo".format(features_map_name))
+        mongodb.close()
+
+    def load_features(self):
+        self.features = pd.DataFrame()
+        for features_map_name in self._map['features_maps']:
+            features_map = self.get_features_map(features_map_name)
+            features = Features(features_map,self.start,self.end,self.symbol,self.period)
+            self.features = self.features.join(features.df,how='outer')
+        self.features.index.name = None
+        self.features.dropna(inplace=True)
+    
+    def load_labels(self):
+        pass
