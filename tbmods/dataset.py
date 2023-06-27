a19e9e2759c2a29e31ee55a6ef40ac3695f84786
Boris Slav <boris@slav.rocks>
2023-06-27 09:36:33 +0000

    tbmods - dataset fix events index for labels

--- tbmods/dataset.py
+++ tbmods/dataset.py
@@ -19,6 +19,7 @@ class Dataset:
         self.period = period
         self.load_features()
         self.load_events()
+        self.load_labels()
 
     def get_map(self):
         mongodb = MongoDB()
@@ -48,7 +49,8 @@ class Dataset:
     def load_events(self):
         chartist_features_map = self.get_features_map('talib_pattern_recognition')
         chartist_features = Features(chartist_features_map,self.start,self.end,self.symbol,self.period)
-        self.events = chartist_features.df.index.intersection(self.features.index)
+        self.events = chartist_features.df
+        self.events.index = self.events.index.intersection(self.features.index)
     
     def load_labels(self):
         triple_barrier = TripleBarrier(self.features['close!lag=0'],self.events)
