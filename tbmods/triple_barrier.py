5f4e60d4e670b2b21e7e166593364d583b7e1a40
Boris Slav <boris@slav.rocks>
2023-06-27 09:38:03 +0000

    tbmods - triple_barrier methods fix code increase performance

--- tbmods/triple_barrier.py
+++ tbmods/triple_barrier.py
@@ -5,49 +5,40 @@ import numpy as np
 config = Config()
 
 class TripleBarrier:
-    
+
     def __init__(self,close,events):
         self.close = close
-        self.barriers = pd.DataFrame({"close":self.close.loc[events.index].values},index=events.index)
+        self.barriers = pd.DataFrame(self.close.loc[events.index].values,columns=['close'],index=events.index)
         self.get_horizontal_barriers(config['tbm_up_thresh'],config['tbm_down_thresh'])
         self.get_vertical_barrier()
         self.get_first_touch()
-        self.get_sides()
+        self.get_side()
         
     def get_horizontal_barriers(self,up_thresh,down_thresh):
-        top = []
-        bot = []
-        for date,row in self.barriers.iterrows():
-            price = row.close
-            top.append(price+(price*float(up_thresh)))
-            bot.append(price-(price*float(down_thresh)))
-        self.barriers['top'] = top
-        self.barriers['bot'] = bot
-        self.barriers.dropna(inplace=True)
-        
+        self.barriers['top'] = (self.barriers.close+(self.barriers.close*(float(up_thresh))))
+        self.barriers['bot'] = (self.barriers.close-(self.barriers.close*(float(up_thresh))))
+
     def get_vertical_barrier(self):
-        vertical = self.barriers.index + pd.Timedelta(hours=3)
-        # drop last row because cant guess future event time
-        self.barriers['vertical'] = vertical
-        self.barriers.drop(self.barriers.tail(3).index,inplace=True)
+        self.barriers['vertical'] = self.barriers.index + pd.Timedelta(hours=9)
+        # create new index to remove missing vertical rows due to binance outage and last row because last vertical not exists yet
+        index = pd.DatetimeIndex(self.barriers.vertical).intersection(self.barriers.index)[:-1]
+        self.barriers = self.barriers.loc[index]
         
     def get_first_touch(self):
-        first_touchs = []
-        for date,row in self.barriers.iterrows():
-            price_range = self.close.loc[date:row.vertical]
-            touchs = [row.vertical]
-            if price_range.max() >= row.top: touchs.append(price_range.idxmax())
-            elif price_range.min() <= row.bot: touchs.append(price_range.idxmin())
-            first_touchs.append(min(pd.DatetimeIndex(touchs)))
-        self.barriers['first_touch'] = first_touchs
-    
-    def get_sides(self):
-        sides = []
+        def get_row_first_touch(row):
+            touchs = []
+            price_range = self.close.loc[row.name:row.vertical]
+            touchs.append(row.vertical)
+            if row.top <= price_range.max(): touchs.append(price_range.idxmax())
+            if row.bot >= price_range.min(): touchs.append(price_range.idxmin())
+            return min(pd.DatetimeIndex(touchs))
+        self.barriers['first_touch'] = self.barriers.apply(get_row_first_touch,axis=1)
         self.barriers['close_touch'] = self.close.loc[self.barriers.first_touch].values
-        self.barriers['ret'] = (self.barriers.close_touch - self.barriers.close)/self.barriers.close
-        for date,row in self.barriers.iterrows():
-            if row['close_touch'] >= row.top: sides.append(1)
-            elif row['close_touch'] <= row.bot: sides.append(-1)
-            else: sides.append(np.nan)
-        self.barriers['side'] = sides
+    
+    def get_side(self):
+        def get_row_side(row):
+            if row.close_touch >= row.top: return 1
+            elif row.close_touch <= row.bot: return -1
+            else: return np.nan
+        self.barriers['side'] = self.barriers.apply(get_row_side,axis=1)
         self.barriers.dropna(inplace=True)
