7a47bb905cd5ac7a387e36f50a099b6285cb4be2
Boris Slav <boris@slav.rocks>
2023-07-14 16:47:49 +0000

    trainer, tbmods - implement random_forest model

--- apps/trainer/routers/models.py
+++ apps/trainer/routers/models.py
@@ -1,15 +1,8 @@
-from tbmods.darwin.ichimoku import DarwinIchimoku
-from tbmods.models.ichimoku import ModelIchimoku
+from trainer.routers.datasets import get_dataset_features_maps_by_name
+from tbmods.models.random_forest import ModelRandomForest
 from fastapi import APIRouter, HTTPException
-from tbmods.darwin.tech import DarwinTech
-from tbmods.models.tech import ModelTech
 from tbmods.config import Config
-from tbmods.cache import Cache
 from tbmods.log import Log
-import pandas as pd
-import numpy as np
-import requests
-import json
 
 router = APIRouter(
     prefix="/models",
@@ -19,31 +12,13 @@ router = APIRouter(
 config = Config()
 log = Log(config['app'])
 
-@router.get('/{prefix}/train/{symbol}/{period}/{start}/{end}')
-def train(prefix,symbol,period,start,end):
-    features_list = config['{}_features_selected'.format(prefix)].split(',')
-    if prefix == 'tech': model = ModelTech(symbol,period,start,end,features_list)
-    if prefix == 'ichimoku': model = ModelIchimoku(symbol,period,start,end,features_list)
-    model.update_status(False)
-    model.update_status({"Load dataset":"..."})
-    model.load_dataset()
-    model.update_status({"Load dataset":"OK"})
-    model.clf_init(json.loads(config['tech_clf_config']))
-    model.update_status({"Fit":"..."})
-    model.fit(True)
-    model.save_meta()
-    model.update_status({"Fit":"OK"})
-    model.save_model()
-    model.save_scaler()
-    model.update_status(False)
 
-@router.get('/{prefix}/check_run')
-def check_run(prefix):
-    cache = Cache(config['app'])
-    return cache.data["models/{}/status".format(prefix)]
-    
-@router.get('/{prefix}/darwin/{symbol}/{period}/{start}/{end}')
-def get_darwin(prefix,symbol,period,start,end):
-    if prefix == 'tech': darwin = DarwinTech(symbol,period,start,end)
-    if prefix == 'ichimoku': darwin = DarwinIchimoku(symbol,period,start,end)
-    darwin.evolve()
+@router.get('/train/random_forest/{dataset_name}/{symbol}/{save}')
+def train(dataset_name,symbol,save):
+    """
+    Train random_forest model
+    save : 0 or 1
+    """
+    model = ModelRandomForest(dataset_name,symbol)
+    model.train()
+    if bool(int(save)): model.save()
--- tbmods/models/random_forest.py
+++ tbmods/models/random_forest.py
@@ -1,12 +1,32 @@
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from tbmods.dataset import Dataset
 from tbmods.mongodb import MongoDB
+import joblib
 
 class ModelRandomForest:
     
-    def __init__(self,dataset):
-        self.dataset = dataset
+    def __init__(self,dataset_name,symbol,_map=None):
+        self.symbol = symbol
+        self.dataset_name = dataset_name
+        self.dataset = Dataset(self.dataset_name,None,None,self.symbol,'historical')
+        self.y = self.dataset.labels
+        self.X = self.dataset.features.loc[self.y.index]
+        self.model = RandomForestClassifier(verbose=True)
     
-    def load_map(self):
-        mongodb = MongoDB()
-        self._map = mongodb.find('TB','models_map',{'name':'random_forest'})[0]
-        print(self._map)
+    def train(self):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y)
+        self.model.fit(self.X_train,self.y_train)
+        self.get_perfs()
+        
+    def save(self):
+        joblib.dump(self.model,'/var/cache/models/random_forest_{}_{}'.format(self.dataset_name,self.symbol))
+        
+    def get_perfs(self):
+        self.test_prediction = self.model.predict(self.X_test)
+        self.perfs = {}
+        self.perfs['score'] = self.model.score(self.X_test,self.y_test)
+        self.perfs['confusion_matrix'] = confusion_matrix(self.y_test, self.test_prediction)
+        self.perfs['accuracy'] = accuracy_score(self.y_test, self.test_prediction)
+        self.perfs['f1_score'] = f1_score(self.y_test,self.test_prediction)
