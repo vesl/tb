8ca4ef13b350fe4ae30c48d86953d23f0b218be2
Boris Slav <boris@slav.rocks>
2023-07-19 14:32:31 +0000

    tbmods - models add cv score to random forest

--- tbmods/models/random_forest.py
+++ tbmods/models/random_forest.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from tbmods.dataset import Dataset
 from tbmods.mongodb import MongoDB
@@ -24,7 +24,7 @@ class ModelRandomForest:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y)
         self.model.fit(self.X_train,self.y_train)
         self.get_perfs()
-        
+
     def save(self):
         joblib.dump(self.model,'/var/cache/models/random_forest_{}_{}'.format(self.dataset_name,self.symbol))
         
@@ -32,7 +32,8 @@ class ModelRandomForest:
         self.test_prediction = self.model.predict(self.X_test)
         self.perfs = {}
         self.perfs['score'] = self.model.score(self.X_test,self.y_test)
-        self.perfs['confusion_matrix'] = confusion_matrix(self.y_test, self.test_prediction)
+        self.perfs['confusion_matrix'] = confusion_matrix(self.y_test, self.test_prediction).tolist()
         self.perfs['accuracy'] = accuracy_score(self.y_test, self.test_prediction)
         self.perfs['f1_score'] = f1_score(self.y_test,self.test_prediction)
         self.perfs['training_time'] = datetime.now() - self.time_start
+        self.perfs['cross_val_score'] = cross_val_score(self.model,self.X,self.y).tolist()
